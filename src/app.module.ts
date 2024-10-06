11b16e179a442aef2a366d65f9a48a905dc0469d
Marko Anic <anicmarko9>
2024-10-06 16:38:04 +0200

    init: core, task, util modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { BaseModule } from '@Base/base.module';
+import { UtilModule } from '@Utils/util.module';
+import { TaskModule } from '@Tasks/task.module';
+import { CoreModule } from '@Core/core.module';
 
-@Module({ imports: [BaseModule] })
+@Module({ imports: [BaseModule, UtilModule, TaskModule, CoreModule] })
 export class AppModule {}
