f46ab016c5593d7f7d7b7ee990eafd01785a786f
Marko Anic <anicmarko9>
2024-10-10 09:26:45 +0200

    test: enable e2e health test

--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -1,15 +1,11 @@
-import {
-  type INestApplication,
-  // ValidationPipe,
-  //  VersioningType
-} from '@nestjs/common';
+import { type INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
 import { TestingModule, Test } from '@nestjs/testing';
-// import * as bodyParser from 'body-parser';
-// import * as cookieParser from 'cookie-parser';
-// import * as compression from 'compression';
-// import helmet from 'helmet';
-// import { ConfigService } from '@nestjs/config';
-// import * as request from 'supertest';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
+import * as request from 'supertest';
 
 import { AppModule } from '@/app.module';
 
@@ -23,24 +19,24 @@ describe('HealthController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
 
-    // app.setGlobalPrefix('api');
-    // app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+    app.setGlobalPrefix('api');
+    app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
 
-    // app.use(bodyParser.json({ limit: '50mb' }));
-    // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-    // app.use(cookieParser());
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    app.use(cookieParser());
 
-    // app.use(compression());
+    app.use(compression());
 
-    // const configService: ConfigService = app.get(ConfigService);
-    // app.enableCors({ origin: configService.get<string>('FRONT_URL'), credentials: true });
-    // app.use(helmet());
+    const configService: ConfigService = app.get(ConfigService);
+    app.enableCors({ origin: configService.get<string>('FRONT_URL'), credentials: true });
+    app.use(helmet());
 
-    // app.useGlobalPipes(
-    //   new ValidationPipe({ whitelist: true, transform: true, transformOptions: { enableImplicitConversion: true } }),
-    // );
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true, transformOptions: { enableImplicitConversion: true } }),
+    );
 
-    // app.enableShutdownHooks();
+    app.enableShutdownHooks();
 
     await app.init();
   });
@@ -51,16 +47,14 @@ describe('HealthController (e2e)', () => {
 
   describe('/api/v1/health/database (GET)', () => {
     it('should return 200 because database is up and running', async () => {
-      // const response = await request(app.getHttpServer()).get('/api/v1/health/database');
-      // expect(response.status).toEqual(200);
-      // expect(response.body).toEqual({
-      //   status: 'ok',
-      //   info: { database: { status: 'up' } },
-      //   error: {},
-      //   details: { database: { status: 'up' } },
-      // });
-
-      console.log('This is console logged because there is no deployed DB server yet...');
+      const response = await request(app.getHttpServer()).get('/api/v1/health/database');
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({
+        status: 'ok',
+        info: { database: { status: 'up' } },
+        error: {},
+        details: { database: { status: 'up' } },
+      });
     });
   });
 
