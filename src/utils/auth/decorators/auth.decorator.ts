e457e038f18b60b339e7cfc954dd06d5a1d5c058
Marko Anic <anicmarko9>
2024-10-17 13:16:45 +0200

    refactor: change name of the user decorator

--- src/utils/auth/decorators/auth.decorator.ts
+++ src/utils/auth/decorators/auth.decorator.ts
@@ -8,19 +8,21 @@ import { Request } from 'express';
 
 import { CookiePayload, RequestWithCookie } from '@Auth/interfaces/auth.interface';
 
-export const User = createParamDecorator((data: unknown, ctx: ExecutionContext): CookiePayload => {
-  const req: RequestWithCookie = ctx.switchToHttp().getRequest();
+export const UserDecorator = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): CookiePayload => {
+    const req: RequestWithCookie = ctx.switchToHttp().getRequest();
 
-  const user: CookiePayload = req.user;
+    const user: CookiePayload = req.user;
 
-  if (!user || !user.sub)
-    throw new UnauthorizedException('Request object without user found unexpectedly.');
+    if (!user || !user.sub)
+      throw new UnauthorizedException('Request object without user found unexpectedly.');
 
-  // after user accepts an invitation, he'll be logged in - but he never created his own password beforehand...
-  if (!user.verified) throw new BadRequestException('Password not selected.');
+    // after user accepts an invitation, he'll be logged in - but he never created his own password beforehand...
+    if (!user.verified) throw new BadRequestException('Password not selected.');
 
-  return user;
-});
+    return user;
+  },
+);
 
 // used only once in the codebase - when 1 user invites another user to join his company
 export function assertHasReq(req: Request): asserts req is RequestWithCookie {
